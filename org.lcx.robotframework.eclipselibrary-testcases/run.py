1b0d884e8871b3d16dbd399a3967abff78d06627
laurent.carbonnaux <laurent.carbonnaux@2e4ca1bc-1646-b92e-8251-cd7c479093aa>
2010-07-16 10:00:58 +0000

    Robotframework Eclipselibrary Test Cases initial import

---
+++ org.lcx.robotframework.eclipselibrary-testcases/run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from glob import glob
+
+def main(args):
+    dir = os.path.dirname(__file__)
+    jars = glob(os.path.join(dir, 'lib', '*.jar'))
+    os.environ['CLASSPATH'] = os.pathsep.join(jars)
+    outputdir = os.path.join(dir, 'results')
+    os.system("jybot --debugfile jybot.log --loglevel TRACE --outputdir \"%s\" %s" % (outputdir, ' '.join(args)))
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
