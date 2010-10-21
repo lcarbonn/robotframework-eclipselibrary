04a4b6ad38843b443d9ef03609407d8da95ccc91
laurent.carbonnaux <laurent.carbonnaux@2e4ca1bc-1646-b92e-8251-cd7c479093aa>
2010-10-21 20:41:18 +0000

    Robotframework Eclipselibrary v0.10

--- org.lcx.robotframework.swtbotplugin/src/org/lcx/robotframework/swtbotplugin/service/ISwtbotService.java
+++
@@ -1,9 +0,0 @@
-package org.lcx.robotframework.swtbotplugin.service;
-
-import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
-
-public interface ISwtbotService {
-
-	public SWTWorkbenchBot getSWTWorkbenchBot();
-	
-}
