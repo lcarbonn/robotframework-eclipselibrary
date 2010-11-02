ac02d8aae4f7719dfa47fe27de6845e16eb155d7
laurent.carbonnaux <laurent.carbonnaux@2e4ca1bc-1646-b92e-8251-cd7c479093aa>
2010-11-02 18:21:24 +0000

    Robotframework Eclipselibrary v0.11 test cases with RF standalone Jar

--- trunk/org.lcx.robotframework.eclipselibrary-testcases/TestSamples/eclipsebot.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set ECLIPSE_LIBRARY=lib/eclipselibrary-0.11-with-dependencies.jar
-set ROBOTFRAMEWORK=robotframework-2.5.4.1.jar
-java -Xms256m -Xmx512m -XX:PermSize=64M -XX:MaxPermSize=256M  -Xbootclasspath/a:%ECLIPSE_LIBRARY% -jar  %ROBOTFRAMEWORK% --debugfile jybot.log --loglevel TRACE --outputdir results %* 
