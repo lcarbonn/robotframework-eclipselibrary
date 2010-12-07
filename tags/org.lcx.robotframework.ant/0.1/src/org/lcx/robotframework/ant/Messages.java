2e4cb73fd0f6f12f4c575d0825ad351bbe20b28c
laurent.carbonnaux <laurent.carbonnaux@2e4ca1bc-1646-b92e-8251-cd7c479093aa>
2010-12-07 21:23:03 +0000

    RobotAnt v0.1

---
+++ tags/org.lcx.robotframework.ant/0.1/src/org/lcx/robotframework/ant/Messages.java
@@ -0,0 +1,31 @@
+package org.lcx.robotframework.ant;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+public class Messages {
+	private static final String BUNDLE_NAME = "robotant"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
+			.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+	
+	public static boolean getBoolean(String key) {
+		try {
+			return Boolean.parseBoolean(RESOURCE_BUNDLE.getString(key));
+		} catch (MissingResourceException e) {
+			return false;
+		}
+	}
+	
+}
