1324c79cbc58f50ee08b53588e13daea90160544
laurent.carbonnaux <laurent.carbonnaux@2e4ca1bc-1646-b92e-8251-cd7c479093aa>
2011-03-13 08:48:52 +0000

    Robotframework Eclipselibrary v0.15

--- trunk/org.lcx.robotframework.eclipselibrary-testcases/eclipsebot.bat
+++ trunk/org.lcx.robotframework.eclipselibrary-testcases/eclipsebot.bat
@@ -1,4 +1,4 @@
 @echo off
-set ECLIPSE_LIBRARY=dist/eclipselibrary-0.14-with-dependencies-e36.jar
+set ECLIPSE_LIBRARY=dist/eclipselibrary-0.15-with-dependencies-e36.jar
 set ROBOTFRAMEWORK=libext/robotframework-2.5.6.jar
 java -Xmx512m -XX:MaxPermSize=256M  -Xbootclasspath/a:%ECLIPSE_LIBRARY% -jar  %ROBOTFRAMEWORK% --debugfile jybot.log --loglevel TRACE --outputdir results %* 
