9006fd9d3c4b504236accb9ca86646431fb6fb15
laurent.carbonnaux <laurent.carbonnaux@2e4ca1bc-1646-b92e-8251-cd7c479093aa>
2013-05-13 20:56:18 +0000

    eclipselibrary 0.18

--- org.lcx.robotframework.eclipselibrary/src/org/lcx/robotframework/eclipse/keyword/launch/EclipseLaunchingKeywords.java
+++ org.lcx.robotframework.eclipselibrary/src/org/lcx/robotframework/eclipse/keyword/launch/EclipseLaunchingKeywords.java
@@ -1,17 +1,20 @@
 /*
- * Copyright 2010 L. Carbonnaux
+ * Copyright 2013 L. Carbonnaux
  */
 
 package org.lcx.robotframework.eclipse.keyword.launch;
 
 import java.lang.Thread.UncaughtExceptionHandler;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URLClassLoader;
 import java.security.Permission;
 
 import org.apache.log4j.Logger;
 import org.lcx.robotframework.eclipse.LibraryLogger;
 import org.lcx.robotframework.eclipse.bridge.SWTBotBridge;
 import org.lcx.robotframework.eclipse.bridge.SWTBotBridgeException;
+import org.lcx.robotframework.eclipse.utils.EclipseUtil;
 import org.robotframework.javalib.annotation.ArgumentNames;
 import org.robotframework.javalib.annotation.RobotKeyword;
 import org.robotframework.javalib.annotation.RobotKeywords;
@@ -21,11 +24,10 @@ public class EclipseLaunchingKeywords {
 
 	private static final String TIMEOUT = "-timeout"; //$NON-NLS-1$
 	private boolean error = false;
-	private final static String ECLIPSE_LAUNCHER = "org.eclipse.equinox.launcher.Main";
+	private final static String ECLIPSE_LAUNCHER_CLASS = "org.eclipse.equinox.launcher.Main";
 	
 	private Logger log = LibraryLogger.getLogger();
 
-	 // install security manager to avoid System.exit() call from lib
 	public EclipseLaunchingKeywords() {
 	}
 	
@@ -48,16 +50,28 @@ public class EclipseLaunchingKeywords {
         boolean isBridgeInitialized = false;
         boolean timeoutReached = false;
 
+		// Searching and loading the launcher jar of eclipse in the -install folder
+        log.debug("loading eclipse launcher jar");
+		EclipseUtil eu = new EclipseUtil();
+		try {
+			eu.loadEclipseLauncherJar(args);
+		} catch (MalformedURLException e1) {
+			throw new SWTBotBridgeException(e1);
+		}
+		// (only) Search the main method
+		log.debug("get the Main method");
         getMainMethod();
     	
+        // Prepare the exception handler
 		UncaughtExceptionHandler eh = new UncaughtExceptionHandler() {
-			
+
 			public void uncaughtException(Thread t, Throwable e) {
 				error=true;
 				throw new RuntimeException(e);
 			}
 		};
 
+		// Prepare the thread in which eclipse will be started
     	Thread et = createThread(new Runnable() {
             public void run() {
                 try {
@@ -71,25 +85,24 @@ public class EclipseLaunchingKeywords {
     	et.setUncaughtExceptionHandler(eh);
 		log.info("Eclipse start is requested");
 
+		// Change the eclipse escurity manager in order to be able to launch it
 		SecurityManager previousSecurityManager = setSecurityManager();
 
+		// Start the thread Eclipse
 		et.start();
 
-		long timeout = 2 * 60 * 1000;
-        for (int i = 0; i < args.length; i++) {
-			if(args[i].equals(TIMEOUT) && i<(args.length-1)) {
-				timeout = Long.parseLong(args[i+1]);
-			}
-		}
-
+		// Get the timeout parameter
+		long timeout = getTimeOut(args);
+		
         long start = System.currentTimeMillis();
         long end = start;
+        // Wait for the bridge to get initialized
         while(!error && !isBridgeInitialized && !timeoutReached) {
 			// Is bridge initialized?
         	try {
             	Thread.sleep(1000);
 			} catch (Exception e) {
-				// nothing tod o
+				// nothing to do
 			}
         	isBridgeInitialized = SWTBotBridge.isISBRIDGEINITIATED();
         	end = System.currentTimeMillis();
@@ -112,10 +125,25 @@ public class EclipseLaunchingKeywords {
         			"  Or use -timeout option in Start Eclipse keyword");
         }
         
+        // reset eclipse security manager to previous one
         resetSecurityManager(previousSecurityManager);
         
     	return et;
     }
+
+	private long getTimeOut(String[] args) {
+		long timeout = 2 * 60 * 1000;
+		if(args != null) {
+	        for (int i = 0; i < args.length; i++) {
+				if(args[i].equals(TIMEOUT) && i<(args.length-1)) {
+					timeout = Long.parseLong(args[i+1]);
+				}
+			}
+		}
+		return timeout;
+	}
+
+
 
 	private Thread createThread(Runnable runnable) {
     	Thread t = new Thread(runnable);
@@ -124,10 +152,13 @@ public class EclipseLaunchingKeywords {
     
     private void internalLaunchEclipse(String[] args) throws SWTBotBridgeException {
 		try {
+			log.debug("requesting Main method of eclipse launcher jar");
             Method mainMethod = getMainMethod();
             mainMethod.invoke(null, new Object[] { args });
 		} catch (Exception e) {
     		error = true;
+    		log.debug("Error in internalLaunchEclipse");
+    		log.error(e);
     		log.error(e.getCause().getMessage(), e.getCause());
 			throw new SWTBotBridgeException(e.getCause().getMessage(), e);
 		}
@@ -135,12 +166,15 @@ public class EclipseLaunchingKeywords {
     
     private Method getMainMethod() throws SWTBotBridgeException {
         try {
-            Class<?> clss = Class.forName(ECLIPSE_LAUNCHER);
-            return clss.getMethod("main", String[].class);
+            //Class<?> clss = Class.forName(ECLIPSE_LAUNCHER);
+        	URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
+            //return clss.getMethod("main", String[].class);
+        	Class<?> clss = urlClassLoader.loadClass(ECLIPSE_LAUNCHER_CLASS);
+        	return clss.getMethod("main", String[].class);
         } catch (NoSuchMethodException e) {
-            throw new SWTBotBridgeException("Class '" + ECLIPSE_LAUNCHER + "' doesn't have a main method.");
+            throw new SWTBotBridgeException("Class '" + ECLIPSE_LAUNCHER_CLASS + "' doesn't have a main method.");
         } catch (ClassNotFoundException e) {
-            throw new SWTBotBridgeException("Class '" + ECLIPSE_LAUNCHER + "' not found.");
+            throw new SWTBotBridgeException("Class '" + ECLIPSE_LAUNCHER_CLASS + "' not found.");
         }
     }
 
@@ -149,7 +183,7 @@ public class EclipseLaunchingKeywords {
 	}
 
 	private SecurityManager setSecurityManager() {
-		// install security manager to avoid System.exit() call from lib
+		// install security manager to avoid System.exit() called from lib
 		SecurityManager previousSecurityManager = System.getSecurityManager();
 
 		final SecurityManager securityManager         = new SecurityManager() {
---
+++ org.lcx.robotframework.eclipselibrary/testsrc/org/lcx/robotframework/eclipse/utils/EclipseLaunchingTest.java
@@ -0,0 +1,75 @@
+package org.lcx.robotframework.eclipse.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.net.URL;
+
+import org.junit.Test;
+import org.lcx.robotframework.eclipse.keyword.launch.EclipseLaunchingKeywords;
+
+
+public class EclipseLaunchingTest {
+
+	/*
+	try to add eclipse in classpath
+	 */
+	
+
+	@Test
+	public void testFindEclipseMain() throws Exception {
+		long t1 = System.currentTimeMillis();
+
+		String pathToEclipse = "file://D:/eclipse/eclipse-jee-helios-SR2-win32_OLD/eclipse";
+		
+		// find the jar
+		URL url = new URL(pathToEclipse);
+		
+		EclipseUtil eclipseUtil = new EclipseUtil();
+		
+		File eclipseLauncherJar = eclipseUtil.findTheJar(url.getFile());
+		System.out.println(eclipseLauncherJar.getAbsolutePath());
+		assertNotNull(eclipseLauncherJar);
+		assertEquals("org.eclipse.equinox.launcher_1.1.1.R36x_v20101122_1400.jar", eclipseLauncherJar.getName());
+		
+
+		
+		// load the jar
+		eclipseUtil.loadTheJar(eclipseLauncherJar);
+		
+	
+		long t2 = System.currentTimeMillis();
+		System.out.println(t2-t1);
+		
+	}
+	
+	@Test
+	public void testLoadEclipse() throws Exception {
+		long t1 = System.currentTimeMillis();
+
+		String pathToEclipse = "file:/D:/eclipse/eclipse-jee-helios-SR2-win32_OLD/eclipse";
+		String pathToWorkspace = "file:/D:/bootstrap36-robot/worskspace_blank";
+		String[] args = new String[5];
+		args[0]="-install";
+		args[1]=pathToEclipse;
+		args[2]="-data";
+		args[3]=pathToWorkspace;
+		args[4]="-consolelog -debug";
+//		EclipseUtil eclipseUtil = new EclipseUtil();
+//		eclipseUtil.loadEclipseLauncherJar(args);
+		
+		// launch eclipse
+		EclipseLaunchingKeywords ekw = new EclipseLaunchingKeywords();
+		try {
+			ekw.startEclipse(args);
+			Thread.sleep(10000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		long t2 = System.currentTimeMillis();
+		System.out.println(t2-t1);
+	}
+}
