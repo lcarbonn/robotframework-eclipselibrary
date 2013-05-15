f7a51b3112f6838b1220ba518a120700e15f7a55
laurent.carbonnaux <laurent.carbonnaux@2e4ca1bc-1646-b92e-8251-cd7c479093aa>
2013-05-15 20:25:20 +0000

    eclipselibrary 0.19

---
+++ trunk/org.lcx.robotframework.eclipselibrary/libdoc.bat
@@ -0,0 +1,7 @@
+@echo off
+set JYTHONPATH=C:\RobotFramework\Python26\Lib\site-packages
+set CLASSPATH=C:\RobotFramework\TestEclipse\dist\eclipselibrary-0.19-with-dependencies.jar
+set jython=C:\RobotFramework\jython2.5.1\jython.bat
+set docpath=C:\RobotFramework\TestEclipse\documentation
+rem %jython% -m robot.libdoc --version 0.19 org.lcx.robotframework.eclipse.EclipseLibrary %docpath%\EclipseLibrary.html
+%jython% -m robot.libdoc --version 0.19 org.lcx.robotframework.eclipse.EclipseLibrary %docpath%\EclipseLibrary.xml
