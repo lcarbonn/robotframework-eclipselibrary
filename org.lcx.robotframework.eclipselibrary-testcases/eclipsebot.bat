5dfa4952215efa13c3cc5f6a65d8ff1baaa5acd4
lcarbonn <laurent.carbonnaux@gmail.com>
2015-04-19 17:51:11 +0200

    testsamples

--- org.lcx.robotframework.eclipselibrary-testcases/eclipsebot.bat
+++ org.lcx.robotframework.eclipselibrary-testcases/eclipsebot.bat
@@ -1,4 +1,4 @@
 @echo off
-set ECLIPSE_LIBRARY=dist/eclipselibrary-0.19-with-dependencies.jar
-set ROBOTFRAMEWORK=libext/robotframework-2.7.7.jar
+set ECLIPSE_LIBRARY=dist/eclipselibrary-0.21-with-dependencies.jar
+set ROBOTFRAMEWORK=libext/robotframework-2.8.7.jar
 java -Xmx512m -XX:MaxPermSize=256M  -Xbootclasspath/a:%ECLIPSE_LIBRARY% -jar  %ROBOTFRAMEWORK% --debugfile jybot.log --loglevel DEBUG --outputdir results %* 
