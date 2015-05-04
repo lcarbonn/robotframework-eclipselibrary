db2cbdcd16a8d3b74f97710716c4265834765e72
lcarbonn <laurent.carbonnaux@gmail.com>
2015-05-04 21:14:59 +0200

    libdoc

--- org.lcx.robotframework.eclipselibrary/libdoc.bat
+++ org.lcx.robotframework.eclipselibrary/libdoc.bat
@@ -1,7 +1,6 @@
 @echo off
-rem set JYTHONPATH=C:\RobotFramework\Python26\Lib\site-packages
-set CLASSPATH=C:\RobotFramework\TestEclipse\dist\eclipselibrary-0.19-with-dependencies.jar
-set jython=C:\RobotFramework\jython2.5.1\jython.bat
-set docpath=C:\RobotFramework\TestEclipse\documentation
-rem %jython% -m robot.libdoc --version 0.19 org.lcx.robotframework.eclipse.EclipseLibrary %docpath%\EclipseLibrary.html
-%jython% -Dpython.path=C:\RobotFramework\Python26\Lib\site-packages -m robot.libdoc --version 0.19 org.lcx.robotframework.eclipse.EclipseLibrary %docpath%\EclipseLibrary.html
+set CLASSPATH=D:\Dev\RobotFramework\TestEclipse\dist\eclipselibrary-0.20-with-dependencies.jar
+set jython=C:\Tools\jython2.5.4rc1\jython.bat
+set docpath=D:\Dev\RobotFramework\TestEclipse\documentation
+rem %jython% -Dpython.path=C:\Python27\Lib\site-packages -m robot.libdoc --format html --version 0.20 org.lcx.robotframework.eclipse.EclipseLibrary %docpath%\EclipseLibrary.html
+%jython% -Dpython.path=C:\Python27\Lib\site-packages -m robot.libdoc --format xml --version 0.20 org.lcx.robotframework.eclipse.EclipseLibrary %docpath%\EclipseLibrary.xml
